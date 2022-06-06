740c30669b0a8c63440b43692d97a97c8573f658
Gabri3lDev <gabri3lcadoso07@gmail.com>
2022-06-06 03:20:44 -0300

    Edix

---
+++ script.js
@@ -0,0 +1,690 @@
+const edit1 = document.getElementById("my-video-1")
+const edit2 = document.getElementById("my-video-2")
+const edit3 = document.getElementById("my-video-3")
+const edit4 = document.getElementById("my-video-4")
+const edit5 = document.getElementById("my-video-5")
+const edit6 = document.getElementById("my-video-6")
+const edit7 = document.getElementById("my-video-7")
+const edit8 = document.getElementById("my-video-8")
+const edit9 = document.getElementById("my-video-9")
+const edit10 = document.getElementById("my-video-10")
+const edit11 = document.getElementById("my-video-11")
+const edit12 = document.getElementById("my-video-12")
+const edit13 = document.getElementById("my-video-13")
+const edit14 = document.getElementById("my-video-14")
+const edit15 = document.getElementById("my-video-15")
+
+
+// ============================================================= //
+
+const edit16 = document.getElementById("my-video-16")
+const edit17 = document.getElementById("my-video-17")
+const edit18 = document.getElementById("my-video-18")
+const edit19 = document.getElementById("my-video-19")
+const edit20 = document.getElementById("my-video-20")
+const edit21 = document.getElementById("my-video-21")
+const edit22 = document.getElementById("my-video-22")
+const edit23 = document.getElementById("my-video-23")
+const edit24 = document.getElementById("my-video-24")
+const edit25 = document.getElementById("my-video-25")
+const edit26 = document.getElementById("my-video-26")
+const edit27 = document.getElementById("my-video-27")
+const edit28 = document.getElementById("my-video-28")
+const edit29 = document.getElementById("my-video-29")
+const edit30 = document.getElementById("my-video-30")
+
+// ============================================================== //
+
+const edit31 = document.getElementById("my-video-31")
+const edit32 = document.getElementById("my-video-32")
+const edit33 = document.getElementById("my-video-33")
+const edit34 = document.getElementById("my-video-34")
+const edit35 = document.getElementById("my-video-35")
+const edit36 = document.getElementById("my-video-36")
+const edit37 = document.getElementById("my-video-37")
+const edit38 = document.getElementById("my-video-38")
+const edit39 = document.getElementById("my-video-39")
+const edit40 = document.getElementById("my-video-40")
+const edit41 = document.getElementById("my-video-41")
+const edit42 = document.getElementById("my-video-42")
+const edit43 = document.getElementById("my-video-43")
+const edit44 = document.getElementById("my-video-44")
+const edit45 = document.getElementById("my-video-45")
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  effct: 'coverflow',
+  centeredSlides: true,
+
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+    400: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 1.42,
+    },
+    600: {
+      slidesPerView: 1.72,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    800: {
+      slidesPerView: 2.3,
+    },
+    900: {
+      slidesPerView: 2.6,
+    },
+    1000: {
+      slidesPerView: 2.9,
+    },
+    1100: {
+      slidesPerView: 3.2,
+    },
+    1200: {
+      slidesPerView: 3.5,
+    },
+  }
+});
+
+document.getElementById("slide1edit1").addEventListener("dblclick", () => {
+  document.getElementById("pop1").style.display = "flex";
+  edit1.play();
+})
+
+document.getElementById("sair").addEventListener("click", () => {
+  document.getElementById("pop1").style.display = "none";
+  edit1.pause();
+  edit1.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit2").addEventListener("dblclick", () => {
+  document.getElementById("pop2").style.display = "flex";
+  edit2.play();
+})
+
+document.getElementById("sair2").addEventListener("click", () => {
+  document.getElementById("pop2").style.display = "none";
+  edit2.pause();
+  edit2.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit3").addEventListener("dblclick", () => {
+  document.getElementById("pop3").style.display = "flex";
+  edit3.play();
+})
+
+document.getElementById("sair3").addEventListener("click", () => {
+  document.getElementById("pop3").style.display = "none";
+  edit3.pause();
+  edit3.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit4").addEventListener("dblclick", () => {
+  document.getElementById("pop4").style.display = "flex";
+  edit4.play();
+})
+
+document.getElementById("sair4").addEventListener("click", () => {
+  document.getElementById("pop4").style.display = "none";
+  edit4.pause();
+  edit4.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit5").addEventListener("dblclick", () => {
+  document.getElementById("pop5").style.display = "flex";
+  edit5.play();
+})
+
+document.getElementById("sair5").addEventListener("click", () => {
+  document.getElementById("pop5").style.display = "none";
+  edit5.pause();
+  edit5.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit6").addEventListener("dblclick", () => {
+  document.getElementById("pop6").style.display = "flex";
+  edit6.play();
+})
+
+document.getElementById("sair6").addEventListener("click", () => {
+  document.getElementById("pop6").style.display = "none";
+  edit6.pause();
+  edit6.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit7").addEventListener("dblclick", () => {
+  document.getElementById("pop7").style.display = "flex";
+  edit7.play();
+})
+
+document.getElementById("sair7").addEventListener("click", () => {
+  document.getElementById("pop7").style.display = "none";
+  edit7.pause();
+  edit7.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit8").addEventListener("dblclick", () => {
+  document.getElementById("pop8").style.display = "flex";
+  edit8.play();
+})
+
+document.getElementById("sair8").addEventListener("click", () => {
+  document.getElementById("pop8").style.display = "none";
+  edit8.pause();
+  edit8.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit9").addEventListener("dblclick", () => {
+  document.getElementById("pop9").style.display = "flex";
+  edit9.play();
+})
+
+document.getElementById("sair9").addEventListener("click", () => {
+  document.getElementById("pop9").style.display = "none";
+  edit9.pause();
+  edit9.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit10").addEventListener("dblclick", () => {
+  document.getElementById("pop10").style.display = "flex";
+  edit10.play();
+})
+
+document.getElementById("sair10").addEventListener("click", () => {
+  document.getElementById("pop10").style.display = "none";
+  edit10.pause();
+  edit10.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit11").addEventListener("dblclick", () => {
+  document.getElementById("pop11").style.display = "flex";
+  edit11.play();
+})
+
+document.getElementById("sair11").addEventListener("click", () => {
+  document.getElementById("pop11").style.display = "none";
+  edit11.pause();
+  edit11.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit12").addEventListener("dblclick", () => {
+  document.getElementById("pop12").style.display = "flex";
+  edit12.play();
+})
+
+document.getElementById("sair12").addEventListener("click", () => {
+  document.getElementById("pop12").style.display = "none";
+  edit12.pause();
+  edit12.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit13").addEventListener("dblclick", () => {
+  document.getElementById("pop13").style.display = "flex";
+  edit13.play();
+})
+
+document.getElementById("sair13").addEventListener("click", () => {
+  document.getElementById("pop13").style.display = "none";
+  edit13.pause();
+  ediedit13t8.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit14").addEventListener("dblclick", () => {
+  document.getElementById("pop14").style.display = "flex";
+  edit14.play();
+})
+
+document.getElementById("sair14").addEventListener("click", () => {
+  document.getElementById("pop14").style.display = "none";
+  edit14.pause();
+  edit14.currentTime = 0;
+})
+
+//-----
+
+document.getElementById("slide1edit15").addEventListener("dblclick", () => {
+  document.getElementById("pop15").style.display = "flex";
+  edit15.play();
+})
+
+document.getElementById("sair15").addEventListener("click", () => {
+  document.getElementById("pop15").style.display = "none";
+  edit15.pause();
+  edit15.currentTime = 0;
+})
+
+// ================================================================ //
+
+// ================================================================ //
+
+document.getElementById("slide2edit1").addEventListener("dblclick", () => {
+  document.getElementById("pop16").style.display = "flex";
+  edit16.play();
+})
+
+document.getElementById("sair16").addEventListener("click", () => {
+  document.getElementById("pop16").style.display = "none";
+  edit16.pause();
+  edit16.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit2").addEventListener("dblclick", () => {
+  document.getElementById("pop17").style.display = "flex";
+  edit17.play();
+})
+
+document.getElementById("sair17").addEventListener("click", () => {
+  document.getElementById("pop17").style.display = "none";
+  edit17.pause();
+  edit17.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide1edit3").addEventListener("dblclick", () => {
+  document.getElementById("pop18").style.display = "flex";
+  edit18.play();
+})
+
+document.getElementById("sair18").addEventListener("click", () => {
+  document.getElementById("pop18").style.display = "none";
+  edit18.pause();
+  edit18.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit4").addEventListener("dblclick", () => {
+  document.getElementById("pop19").style.display = "flex";
+  edit19.play();
+})
+
+document.getElementById("sair19").addEventListener("click", () => {
+  document.getElementById("pop19").style.display = "none";
+  edit19.pause();
+  edit19.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit5").addEventListener("dblclick", () => {
+  document.getElementById("pop20").style.display = "flex";
+  edit20.play();
+})
+
+document.getElementById("sair20").addEventListener("click", () => {
+  document.getElementById("pop20").style.display = "none";
+  edit20.pause();
+  edit20.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit6").addEventListener("dblclick", () => {
+  document.getElementById("pop21").style.display = "flex";
+  edit21.play();
+})
+
+document.getElementById("sair21").addEventListener("click", () => {
+  document.getElementById("pop21").style.display = "none";
+  edit21.pause();
+  edit21.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit7").addEventListener("dblclick", () => {
+  document.getElementById("pop22").style.display = "flex";
+  edit22.play();
+})
+
+document.getElementById("sair22").addEventListener("click", () => {
+  document.getElementById("pop22").style.display = "none";
+  edit22.pause();
+  edit22.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit8").addEventListener("dblclick", () => {
+  document.getElementById("pop23").style.display = "flex";
+  edit23.play();
+})
+
+document.getElementById("sair23").addEventListener("click", () => {
+  document.getElementById("pop23").style.display = "none";
+  edit23.pause();
+  edit23.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit9").addEventListener("dblclick", () => {
+  document.getElementById("pop24").style.display = "flex";
+  edit24.play();
+})
+
+document.getElementById("sair24").addEventListener("click", () => {
+  document.getElementById("pop24").style.display = "none";
+  edit24.pause();
+  edit24.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit10").addEventListener("dblclick", () => {
+  document.getElementById("pop25").style.display = "flex";
+  edit25.play();
+})
+
+document.getElementById("sair25").addEventListener("click", () => {
+  document.getElementById("pop25").style.display = "none";
+  edit25.pause();
+  edit25.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit11").addEventListener("dblclick", () => {
+  document.getElementById("pop26").style.display = "flex";
+  edit26.play();
+})
+
+document.getElementById("sair26").addEventListener("click", () => {
+  document.getElementById("pop26").style.display = "none";
+  edit26.pause();
+  edit26.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit12").addEventListener("dblclick", () => {
+  document.getElementById("pop27").style.display = "flex";
+  edit27.play();
+})
+
+document.getElementById("sair27").addEventListener("click", () => {
+  document.getElementById("pop27").style.display = "none";
+  edit27.pause();
+  edit27.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit13").addEventListener("dblclick", () => {
+  document.getElementById("pop28").style.display = "flex";
+  edit16.play();
+})
+
+document.getElementById("sair28").addEventListener("click", () => {
+  document.getElementById("pop28").style.display = "none";
+  edit28.pause();
+  edit28.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit14").addEventListener("dblclick", () => {
+  document.getElementById("pop29").style.display = "flex";
+  edit29.play();
+})
+
+document.getElementById("sair29").addEventListener("click", () => {
+  document.getElementById("pop29").style.display = "none";
+  edit29.pause();
+  edit29.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide2edit15").addEventListener("dblclick", () => {
+  document.getElementById("pop30").style.display = "flex";
+  edit30.play();
+})
+
+document.getElementById("sair30").addEventListener("click", () => {
+  document.getElementById("pop30").style.display = "none";
+  edit30.pause();
+  edit30.currentTime = 0;
+})
+
+// ======================================================================== //
+
+// ======================================================================== //
+
+document.getElementById("slide3edit1").addEventListener("dblclick", () => {
+  document.getElementById("pop31").style.display = "flex";
+  edit31.play();
+})
+
+document.getElementById("sair31").addEventListener("click", () => {
+  document.getElementById("pop31").style.display = "none";
+  edit31.pause();
+  edit31.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit2").addEventListener("dblclick", () => {
+  document.getElementById("pop32").style.display = "flex";
+  edit32.play();
+})
+
+document.getElementById("sair32").addEventListener("click", () => {
+  document.getElementById("pop32").style.display = "none";
+  edit32.pause();
+  edit32.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit3").addEventListener("dblclick", () => {
+  document.getElementById("pop33").style.display = "flex";
+  edit33.play();
+})
+
+document.getElementById("sair33").addEventListener("click", () => {
+  document.getElementById("pop33").style.display = "none";
+  edit33.pause();
+  edit33.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit4").addEventListener("dblclick", () => {
+  document.getElementById("pop34").style.display = "flex";
+  edit34.play();
+})
+
+document.getElementById("sair34").addEventListener("click", () => {
+  document.getElementById("pop34").style.display = "none";
+  edit34.pause();
+  edit34.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit5").addEventListener("dblclick", () => {
+  document.getElementById("pop35").style.display = "flex";
+  edit35.play();
+})
+
+document.getElementById("sair35").addEventListener("click", () => {
+  document.getElementById("pop35").style.display = "none";
+  edit35.pause();
+  edit35.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit6").addEventListener("dblclick", () => {
+  document.getElementById("pop36").style.display = "flex";
+  edit36.play();
+})
+
+document.getElementById("sair36").addEventListener("click", () => {
+  document.getElementById("pop36").style.display = "none";
+  edit36.pause();
+  edit36.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit7").addEventListener("dblclick", () => {
+  document.getElementById("pop37").style.display = "flex";
+  edit37.play();
+})
+
+document.getElementById("sair37").addEventListener("click", () => {
+  document.getElementById("pop37").style.display = "none";
+  edit37.pause();
+  edit37.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit8").addEventListener("dblclick", () => {
+  document.getElementById("pop38").style.display = "flex";
+  edit38.play();
+})
+
+document.getElementById("sair38").addEventListener("click", () => {
+  document.getElementById("pop38").style.display = "none";
+  edit38.pause();
+  edit38.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit9").addEventListener("dblclick", () => {
+  document.getElementById("pop39").style.display = "flex";
+  edit39.play();
+})
+
+document.getElementById("sair39").addEventListener("click", () => {
+  document.getElementById("pop39").style.display = "none";
+  edit39.pause();
+  edit39.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit10").addEventListener("dblclick", () => {
+  document.getElementById("pop40").style.display = "flex";
+  edit40.play();
+})
+
+document.getElementById("sair31").addEventListener("click", () => {
+  document.getElementById("pop31").style.display = "none";
+  edit40.pause();
+  edit40.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit11").addEventListener("dblclick", () => {
+  document.getElementById("pop41").style.display = "flex";
+  edit41.play();
+})
+
+document.getElementById("sair41").addEventListener("click", () => {
+  document.getElementById("pop41").style.display = "none";
+  edit41.pause();
+  edit41.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit12").addEventListener("dblclick", () => {
+  document.getElementById("pop42").style.display = "flex";
+  edit42.play();
+})
+
+document.getElementById("sair42").addEventListener("click", () => {
+  document.getElementById("pop42").style.display = "none";
+  edit42.pause();
+  edit42.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit13").addEventListener("dblclick", () => {
+  document.getElementById("pop43").style.display = "flex";
+  edit43.play();
+})
+
+document.getElementById("sair43").addEventListener("click", () => {
+  document.getElementById("pop43").style.display = "none";
+  edit43.pause();
+  edit43.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit14").addEventListener("dblclick", () => {
+  document.getElementById("pop44").style.display = "flex";
+  edit44.play();
+})
+
+document.getElementById("sair44").addEventListener("click", () => {
+  document.getElementById("pop44").style.display = "none";
+  edit44.pause();
+  edit44.currentTime = 0;
+})
+
+//------------------
+
+document.getElementById("slide3edit15").addEventListener("dblclick", () => {
+  document.getElementById("pop45").style.display = "flex";
+  edit45.play();
+  console.log("Sucesso!!!")
+})
+
+document.getElementById("sair45").addEventListener("click", () => {
+  document.getElementById("pop45").style.display = "none";
+  edit45.pause();
+  edit45.currentTime = 0;
+})
